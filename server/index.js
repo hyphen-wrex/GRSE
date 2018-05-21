3b192e638b2e0a40f5df769ef26520b3dff238f1
Ivan Badmaev <hyphen.wrex@gmail.com>
2018-05-21 09:29:23 +0300

    added server routes for change

--- server/index.js
+++ server/index.js
@@ -12,11 +12,34 @@ const db = low(adapter);
 app.use(bodyParser.json());
 app.use(express.static(config.STATIC_PATH));
 app.get('/', (req, res) => res.sendfile('index.html'));
-app.get('/api/users', (req, res) => {
+app.get('/api/users/', (req, res) => {
   const users = db.get('users')
     .value();
 
   res.json(users);
+});
+app.get('/api/users/:username', (req, res) => {
+  const { username } = req.params;
+  const user = db.get('users')
+    .find({ username })
+    .value();
+
+  res.json(user);
+});
+app.put('/api/users/:username', (req, res) => {
+  const { username } = req.params;
+  const { data } = req.body;
+  try {
+    const newEntries = db.update('users', users => users.map(user =>
+      (user.username === username
+        ? data
+        : user)
+    ))
+      .write();
+    res.json({ success: true, data: newEntries });
+  } catch (error) {
+    res.json({ success: false, error });
+  }
 });
 
 app.listen(config.PORT, () =>
